40027f703baaff1d8cbb6f79b154167f7142e562
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-09-07 20:47:20 -0600

    upload3

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
 import Info from "../Info/Info";
+import data from "../../data";
 
 import "./Controls.css";
 
 export default function Controls() {
+  const [dataLength, setDataLength] = useState(0);
   const [num, setNum] = useState(0);
 
-//   decCount = () => setNum(num - 1);
+  const decCount = () => {
+    if (num >= 1) {
+      setNum(num - 1);
+    }
+  };
+  const incCount = () => {
+    if (num < data.length - 1) {
+      setNum(num + 1);
+    }
+  };
+
+  const deleteCard = () => {
+    if (dataLength === 1) {
+    } else {
+      data.splice(num, 1);
+      setDataLength(data.length);
+      decCount();
+    }
+    console.log("running?");
+  };
 
   return (
     <div className="controls_main">
-      <Info num={num} />
+      <Info num={num} data={data.length} />
 
       <div className="control_bar">
-        <button className="move_buttons">
+        <button onClick={decCount} className="move_buttons">
           &#8592; Previous
         </button>
-        <p>{num}</p>
-        console.log(num)
         <div className="buttons_group">
           <button className="change_buttons">Edit</button>
-          <button className="change_buttons">Delete</button>
+          <button onClick={deleteCard} className="change_buttons">
+            Delete
+          </button>
           <button className="change_buttons">New</button>
         </div>
-        <button className="move_buttons">Next &#8594;</button>
+        <button onClick={incCount} className="move_buttons">
+          Next &#8594;
+        </button>
       </div>
     </div>
   );
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,35 +6,44 @@ import data from "../../data";
 export default function Info(props) {
   const { num } = props;
 
+  // const deleteCard = data.findIndex((item) => item.id );
+
+ 
+
   return (
     <div className="info_container">
-      <h1 className="name">
+      <h2 className="count">
+        {num + 1} &#8725; {props.data}
+        {console.log(props.data)}
+        {/* >= 1 ? num : "1" */}
+      </h2>
+      <h2 className="name">
         {data[num].name.first + " " + data[num].name.last}
-      </h1>
+      </h2>
 
       {/* {intialData.map(data => (
           <h1 className="name">{data.name.first + " " + data.name.last}</h1>
         ))} */}
-      <h2 className="count">
-        {num} &#8725; {data.length}
-      </h2>
       <div className="info_body">
         <ul className="person_detail">
           <li>
             <b>From:</b>
+            {" " + data[num].city + "," + " " + data[num].country}
           </li>
           <li>
             <b>Job Title:</b>
+            {" " + data[num].title}
           </li>
           <li>
             <b>Employer:</b>
+            {" " + data[num].employer}
           </li>
         </ul>
         <ol className="movie_list">
           <b>Favorite Movies:</b>
-          <li className="custom_indent"></li>
-          <li className="custom_indent"></li>
-          <li className="custom_indent"></li>
+          <li className="custom_indent"> {data[num].favoriteMovies[0]}</li>
+          <li className="custom_indent"> {data[num].favoriteMovies[1]}</li>
+          <li className="custom_indent"> {data[num].favoriteMovies[2]}</li>
         </ol>
       </div>
     </div>
